54aa0c73477e6f6f2f11a3a10ff6179705e5b5eb
Ermak <ermakovila640@gmail.com>
2023-03-24 18:34:04 +0400

    'without_json'

--- comics_shop/home/models.py
+++ comics_shop/home/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.urls import reverse
+import json
 
 
 class Comics(models.Model):
     title = models.CharField(max_length=100)
     publisher = models.ForeignKey('Publishers', models.PROTECT)
     description = models.TextField(default='description')
-    quantity_purchased = models.IntegerField(default=0) # Количество покупок
+    quantity_purchased = models.IntegerField(default=0)  # Количество покупок
     quantity_pages = models.IntegerField(default=0)
     width = models.IntegerField(default=0)
     height = models.IntegerField(default=0)
-    in_stock = models.IntegerField(default=0) # В наличии
+    in_stock = models.IntegerField(default=0)  # В наличии
     picture = models.ImageField()
     price_default = models.FloatField()
     price_now = models.FloatField()
@@ -23,7 +24,10 @@ class Comics(models.Model):
     def __str__(self):
         return self.title
 
+    # def get_absolute_url(self):
+    #     return f'/{self.id}'
     def get_absolute_url(self):
+        # return f'{self.pk}'
         return reverse('view_comics', kwargs={'view_comics_id': self.pk})
 
 
@@ -33,4 +37,34 @@ class Publishers(models.Model):
     def __str__(self):
         return self.title
 
+
+# class Comics_from_json(models.Model):
+# with open('home/other/comics.json', encoding='utf-8') as file:
+#     data = json.load(file)
+# print(data)
+
 # from home.models import *
+class Comics_from_json(models.Model):
+    title = models.CharField(max_length=255)
+    publisher = models.ForeignKey('Publishers', models.PROTECT)
+    author = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    age_rating = models.CharField(max_length=255)
+    description = models.TextField(default='description')
+    quantity_pages = models.IntegerField(default=0)
+    size = models.CharField(max_length=255)
+    cover = models.ImageField()
+    price = models.FloatField()
+    buy_status = models.BooleanField(default=False)
+
+    # class Meta:
+        # verbose_name_plural = 'Comics'
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return f'/{self.id}'
+    # def get_absolute_url(self):
+    #     # return f'{self.pk}'
+    #     return reverse('view_comics', kwargs={'view_comics_id': self.pk})
--- comics_shop/home/urls.py
+++ comics_shop/home/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('/<int:view_comics_id>', view_comics, name='view_comics'),
-    path('/readmore/<int:readmore_id>', ReadMore.as_view(), name='readmore'),
+    path('<int:view_comics_id>', view_comics, name='view_comics'),
+    path('readmore/<int:readmore_id>', ReadMore.as_view(), name='readmore'),
     path('add_comics/', add_comics, name='add_comics'),
     path('add_publisher/', add_publisher, name='add_publisher'),
     path('cart/', cart, name='cart'),
